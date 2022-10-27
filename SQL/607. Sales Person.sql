fd23aa8be44fec99718df7c49b6424c6874c821d
Harshal Raikwar <harshalraikwar07@gmail.com>
2022-10-27 23:22:48 +0530

    ✨ Add code for 607. Sales Person

---
+++ SQL/607. Sales Person.sql
@@ -0,0 +1,2 @@
+-- https://leetcode.com/problems/sales-person
+SELECT salesperson.name FROM orders o JOIN company c ON (o.com_id = c.com_id AND c.name = 'RED') RIGHT JOIN salesperson ON salesperson.sales_id = o.sales_id WHERE o.sales_id IS NULL;
